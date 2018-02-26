8831c2313c548266531a7ba489da22407017cc1f
Сергей Макаров <sergey.mcarrow@gmail.com>
2018-02-26 21:59:56 +0200

    yo

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            while (true)
+            {
+                int i = rnd.Next(2, 10);
+                int ii = rnd.Next(2, 10);
+                Console.Write($"{i} * {ii} = ");
+                int answer = int.Parse(Console.ReadLine());
+                if (i * ii == answer)
+                    Console.WriteLine("Молодец");
+                else
+                    Console.WriteLine("Тупица");
+                
+            }      
+            
+            
         }
     }
 }
